47efa1c704d0c812d93e5a8ca614b09457137557
Danilo Babin <dbabin@telin.ugent.be>
2017-06-18 22:49:45 +0200

    Base class for data types

---
+++ mxCore/mxDataObject.cpp
@@ -0,0 +1,226 @@
+/*=========================================================================
+ 
+ Program:   mipx
+ Module:    mxDataObject.h
+ 
+ Authors: Danilo Babin.
+ Copyright (c) Danilo Babin.
+ All rights reserved.
+ See Copyright.txt
+ 
+ Licensed under the BSD License 2.0.
+ 
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE. See the above copyright notice for more information.
+ 
+ =========================================================================*/
+
+
+
+#include "mxDataObject.h"
+
+
+
+// Definition of the static members
+int mxDataObject::m_is_exclusive_visibility_on = 0;
+//mxDataObjectFactory* mxDataObject::m_factory = 0;
+
+
+mxDataObject::mxDataObject()
+{
+    this->m_factory = NULL;
+	this->m_is_locked = 0;
+    this->m_is_exclusive_visibility_on = 0;
+    this->m_color[0] = this->m_color[1] = this->m_color[2] = this->m_color[3] = 0;
+}
+
+
+mxDataObject::~mxDataObject()
+{
+	this->Reset();
+}
+
+
+int mxDataObject::CopyFrom(mxObject *object)
+{
+    if(!object) return 0;
+    if(object->GetClassName() != this->GetClassName()) return 0;
+    
+    mxDataObject *data_object = static_cast<mxDataObject*>(object);
+    
+    mxObject::CopyFrom(data_object);
+    this->m_log = data_object->m_log;
+    this->SetColor(data_object->m_color[0], data_object->m_color[1], data_object->m_color[2], data_object->m_color[3]);
+    
+    // Do not copy factory pointer
+    // Do not copy locked indicator
+    // Do not copy exclusive visibility indicator (it's the same for all objects).
+    
+    return 1;
+}
+
+
+int mxDataObject::GetBoundsInWorldCoordinates(double &x_min, double &x_max, double &y_min, double &y_max, double &z_min, double &z_max)
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::GetBoundsInWorldCoordinates()"<<std::endl;
+    z_min = y_min = x_min = 0;
+    z_max = y_max = x_max = 100;
+    return 0;
+}
+
+
+int mxDataObject::GetColor(unsigned char *red, unsigned char *green, unsigned char *blue, unsigned char *a)
+{
+    if(!red || !green || !blue || !a) return 0;
+    *red = m_color[0];
+    *green = m_color[1];
+    *blue = m_color[2];
+    *a = m_color[3];
+    return 1;
+}
+
+
+qmxDataObjectContextMenu* mxDataObject::GetContextMenu()
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::GetContextMenu()"<<std::endl;
+    return NULL;
+}
+
+
+mxDataObjectFactory* mxDataObject::GetFactory()
+{
+    return (this->m_factory);
+}
+
+
+mxString& mxDataObject::GetLog()
+{
+	return m_log;
+}
+
+
+qmxDataObjectPropertiesWidget* mxDataObject::GetPropertiesWidget()
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::GetPropertiesWidget()"<<std::endl;
+    return NULL;
+}
+
+
+int mxDataObject::HasActor(vtkActor *actor)
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::HasActor()"<<std::endl;
+    return 0;
+}
+
+
+int mxDataObject::IsExclusiveVisibilityOn()
+{
+    return this->m_is_exclusive_visibility_on;
+}
+
+
+int mxDataObject::IsLocked()
+{
+    return m_is_locked;
+}
+
+
+int mxDataObject::IsVisibleIn3DView()
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::IsVisibleIn3DView()"<<std::endl;
+    return 0;
+}
+
+
+int mxDataObject::LoadFromFile(const char *file_path, const char *extension)
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::LoadFromFile()"<<std::endl;
+    return 0;
+}
+
+
+int mxDataObject::LoadFromFolder(const char *folder_path, const char *extension)
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::LoadFromFolder()"<<std::endl;
+    return 0;
+}
+
+
+void mxDataObject::Reset()
+{
+    mxObject::Reset();
+    this->m_is_locked = 0;
+    this->m_log.Clear();
+}
+
+
+int mxDataObject::SaveToFile(const char *file_path, const char *extension)
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::SaveToFile()"<<std::endl;
+    return 0;
+}
+
+
+int mxDataObject::SaveToFolder(const char *folder_path, const char *extension)
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::SaveToFolder()"<<std::endl;
+    return 0;
+}
+
+
+void mxDataObject::SetColor(unsigned char red, unsigned char green, unsigned char blue, unsigned char a)
+{
+    m_color[0] = red;
+    m_color[1] = green;
+    m_color[2] = blue;
+    m_color[3] = a;
+}
+
+
+void mxDataObject::SetExclusiveVisibility(int is_visibility_exclusive)
+{
+    this->m_is_exclusive_visibility_on = is_visibility_exclusive;
+}
+
+
+void mxDataObject::SetInteractorFor3DView(vtkRenderWindowInteractor *interactor)
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::SetInteractorFor3DView()"<<std::endl;
+}
+
+
+void mxDataObject::SetLocked(int is_locked)
+{
+	this->m_is_locked = is_locked;
+}
+
+
+void mxDataObject::SetVisibilityIn3DView(int is_visible)
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::SetVisibilityIn3DView()"<<std::endl;
+}
+
+
+void mxDataObject::ShowBasicContextMenu()
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::ShowBasicContextMenu()"<<std::endl;
+}
+
+
+void mxDataObject::ShowContextMenu()
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::ShowContextMenu()"<<std::endl;
+}
+
+
+void mxDataObject::ShowProperties()
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::ShowProperties()"<<std::endl;
+}
+
+
+void mxDataObject::Update()
+{
+    std::cout<<this->GetClassName().Get_C_String()<<" called mxDataObject::Update()"<<std::endl;
+}
---
+++ mxCore/mxDataObject.h
@@ -0,0 +1,183 @@
+ /*=========================================================================
+ 
+ Program:   mipx
+ Module:    mxDataObject.h
+ 
+  Authors: Danilo Babin.
+  Copyright (c) Danilo Babin.
+  All rights reserved.
+  See Copyright.txt
+  
+  Licensed under the BSD License 2.0.
+  
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE. See the above copyright notice for more information.
+  
+  =========================================================================*/
+
+
+
+#if defined(mxDataObject_USE_SOURCE_CODE) || defined(mxCore_USE_SOURCE_CODE)
+    #define mxDataObject_API
+#else
+    #if defined(_MSC_VER)
+        #ifdef mxDataObject_EXPORTS
+            #define mxDataObject_API __declspec(dllexport)
+        #else
+            #define mxDataObject_API __declspec(dllimport)
+        #endif
+    #else
+        #ifdef mxDataObject_EXPORTS
+            #define mxDataObject_API __attribute__((visibility("default")))
+        #else
+            #define mxDataObject_API
+        #endif
+    #endif
+#endif
+
+
+#ifndef mxDataObject_H
+	#define mxDataObject_H
+
+
+
+#include "mxObject.h"
+#include "mxString.h"
+#include <iostream>
+
+
+
+// Pre-declarations.
+class vtkRenderWindowInteractor;
+class vtkActor;
+class qmxDataObjectContextMenu;
+class qmxDataObjectPropertiesWidget;
+//class qmxDataContainer;
+class mxDataObjectFactory;
+
+
+/// Base class for data objects.
+/// Notice: Child class (inheriting from this object) will have a pointer to its context menu (that will
+/// inherit from qmxDataObjectContextMenu in "qmxObject.h") and a pointer to its properties widget (that will inherit
+/// from qmxDataObjectPropertiesWidget in "qmxObject.h").
+/// Notice: Child class (inheriting from this object) will have a map between batch commands and actual methods
+/// of the class. In this way we will be able to call the methods of each class through batch command line. Declaration
+/// of mapper list will be in the following fashion: mxObjectBatchCallMapper<TypeName_Of_Pointer_To_Method>  m_batch_call_mapper;
+
+class mxDataObject_API mxDataObject : public mxObject
+{
+    
+protected:
+    
+    /// Color of the data object in RGBA order.
+    unsigned char m_color[4];
+    
+    /// Pointer to the factory of this class object.
+    mxDataObjectFactory *m_factory; //static mxDataObjectFactory *m_factory;
+    
+    /// Indicates if multiple object of this class can be displayed together.
+    /// If the indicator is set, only a single object can be viewed at a time.
+    /// By default the indicator is not set (multiple objects can be displayed together).
+    static int m_is_exclusive_visibility_on;
+    
+    /// Indicates if the object is in use by a function, so it should not be used for another function (hence, locked).
+    /// Notice: The indicator is controlled (set/reset) on a higher level of the app.
+    /// Example: The indicator will be used on Qt level to allow or refuse the appearance of the context menu.
+    int m_is_locked;
+    
+    /// Logbook of all signatures (records/names) and parameters of functions called on this object.
+    mxString m_log;
+
+public:
+    
+    /// Constructor.
+    mxDataObject();
+    
+    /// Destructor.
+    ~mxDataObject();
+    
+    /// Copy data members of input object.
+    virtual int CopyFrom(mxObject *data_object);
+    
+    /// Get bounds of the object in 3D space. If not applicable (e.g. object is a 2D signal) return fail 0.
+    virtual int GetBoundsInWorldCoordinates(double &x_min, double &x_max, double &y_min, double &y_max, double &z_min, double &z_max);
+    
+    /// Get data object color in RGBA order.
+    int GetColor(unsigned char *red, unsigned char *green, unsigned char *blue, unsigned char *a);
+    
+    /// Get the context menu of the object.
+    virtual qmxDataObjectContextMenu* GetContextMenu();
+    
+    /// Get the factory of this class object.
+    mxDataObjectFactory* GetFactory();
+    
+    /// Get log string of all signatures (records/names) and parameters of functions called on this object.
+    mxString& GetLog();
+    
+    /// Get the properties widget of the object.
+    virtual qmxDataObjectPropertiesWidget* GetPropertiesWidget();
+    
+    /// Check if the actor belongs to the object (used for calling the context menu).
+    virtual int HasActor(vtkActor *actor);
+    
+    /// Check if multiple objects can be displayed at the same time.
+    int IsExclusiveVisibilityOn();
+    
+    /// Check if the object is being used by a function (so it should not be used by another function/method at the same time).
+    int IsLocked();
+    
+    /// Check if the object is visible.
+    virtual int IsVisibleIn3DView();
+    
+    /// Load data object from a file of given extension.
+    /// Note: the "extension" does not necessarily need to be used, but is present if needed.
+    virtual int LoadFromFile(const char *file_path, const char *extension);
+
+    /// Load data object from files of given extension in a given folder.
+    /// Note: the "extension" does not necessarily need to be used, but is present if needed.
+    virtual int LoadFromFolder(const char *folder_path, const char *extension);
+    
+    /// Reset the object.
+    virtual void Reset();
+    
+    /// Save the data object to a file of given extension.
+    /// Note: the "extension" does not necessarily need to be used, but is present if needed.
+    virtual int SaveToFile(const char *file_path, const char *extension);
+    
+    /// Save the data object to file of given extension in a given folder.
+    /// Note: the "extension" does not necessarily need to be used, but is present if needed.
+    virtual int SaveToFolder(const char *folder_path, const char *extension);
+    
+    /// Set data object color in RGBA order.
+    void SetColor(unsigned char red, unsigned char green, unsigned char blue, unsigned char a);
+    
+    /// Set exclusive visibility.
+    void SetExclusiveVisibility(int is_visibility_exclusive);
+    
+    /// Set the interactor for VTK visualization.
+    virtual void SetInteractorFor3DView(vtkRenderWindowInteractor *interactor);
+
+    /// Lock/unlock the object. Use this to indicate that an external function is using the object. Do not forget to release the
+    /// object after the efunction execution has ended.
+    void SetLocked(int is_locked);
+    
+    /// Set the visibility of the object for VTK visualization.
+    virtual void SetVisibilityIn3DView(int is_visible);
+    
+    /// Method that will show basic context menu of the object.
+    virtual void ShowBasicContextMenu();
+    
+    /// This will show context menu of the object.
+    virtual void ShowContextMenu();
+    
+    /// Method that will show the object properties in the properties widget.
+    virtual void ShowProperties();
+    
+    /// Update the visual structures of object after modification.
+    virtual void Update();
+};
+
+
+
+#endif
